50a38f565f17397c88beb2fd112cc295bb96a6eb
atlibby <atlibby@uvm.edu>
2023-01-19 12:14:33 -0500

    Added time logs

--- simulate.py
+++ simulate.py
@@ -1,5 +1,10 @@
 import pybullet as p
+import time
+t = 1/60
 physicsClient = p.connect(p.GUI)
+print ("Start: %s" % time.ctime())
 for i in range(0, 1000):
 	p.stepSimulation()
+	time.sleep(t)
 p.disconnect()
+print ("End: %s" % time.ctime())
