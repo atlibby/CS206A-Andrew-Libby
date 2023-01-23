2a64e8b58642835b57f4318197d58a59dcc0397a
atlibby <atlibby@uvm.edu>
2023-01-23 16:42:00 -0500

    Changed dimensions of cube

--- generate.py
+++ generate.py
@@ -1,1 +1,15 @@
-#Hello
+import pyrosim.pyrosim as pyrosim
+
+pyrosim.Start_SDF("box.sdf")
+
+length = 1
+
+width = 5
+
+height = 1
+
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
+
+pyrosim.End()
+
+
