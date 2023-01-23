3ce9d24e3e8799b7f064519cf6ee69f4aa0ee116
atlibby <atlibby@uvm.edu>
2023-01-23 16:06:20 -0500

    Cube now has some gravity

--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import time
 t = 1/60
 physicsClient = p.connect(p.GUI)
 print ("Start: %s" % time.ctime())
+p.setGravity(0,0,-9.8)
 p.loadSDF("box.sdf")
 for i in range(0, 1000):
 	p.stepSimulation()
