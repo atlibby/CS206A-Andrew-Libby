ff177f6534580e666d64c92ed1108e83630577f3
atlibby <atlibby@uvm.edu>
2023-01-23 16:12:00 -0500

    Cube now sits on floor

--- simulate.py
+++ simulate.py
@@ -1,9 +1,12 @@
+import pybullet_data
 import pybullet as p
 import time
 t = 1/60
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 print ("Start: %s" % time.ctime())
 p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 for i in range(0, 1000):
 	p.stepSimulation()
