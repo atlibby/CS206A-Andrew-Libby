1da3f85623eec8bc9a90615ac46b7bfbc975baae
atlibby <atlibby@uvm.edu>
2023-01-23 17:21:51 -0500

    Cube no longer clips to the ground at generation

--- generate.py
+++ generate.py
@@ -2,13 +2,19 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
 
-length = 1
-
 width = 2
+
+length = 1
 
 height = 3
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
+x = 0
+
+y = 1.5
+
+z = 0
+
+pyrosim.Send_Cube(name="Box", pos=[z,x,y] , size=[length,width,height])
 
 pyrosim.End()
 
