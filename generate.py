1d8315bcf7cc1b16b4c9c5434e03536184b6d02c
atlibby <atlibby@uvm.edu>
2023-01-24 09:32:02 -0500

    Generate.py makes only one box

--- generate.py
+++ generate.py
@@ -14,20 +14,7 @@ y = 0.5
 
 z = 0
 
-for g in range(5):
-	width = 1
-	length = 1
-	height = 1
-	for h in range(5):
-		width = 1
-		length = 1
-		height = 1
-		for i in range(10):
-			pyrosim.Send_Cube(name="Box", pos=[z+g,x+h,y+i], 
-size=[length,width,height])
-			width *= .9
-			length *= .9
-			height *= .9
+pyrosim.Send_Cube(name="Box", pos=[z,x,y], size=[length,width,height])
 
 pyrosim.End()
 
