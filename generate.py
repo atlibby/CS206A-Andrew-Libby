eb7b3858efd9de24331ce25b9ce66e2724e518bd
atlibby <atlibby@uvm.edu>
2023-01-24 09:15:20 -0500

    Cubes form multiple towers

--- generate.py
+++ generate.py
@@ -1,20 +1,33 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-width = 2
+width = 1
 
 length = 1
 
-height = 3
+height = 1
 
 x = 0
 
-y = 1.5
+y = 0.5
 
 z = 0
 
-pyrosim.Send_Cube(name="Box", pos=[z,x,y] , size=[length,width,height])
+for g in range(5):
+	width = 1
+	length = 1
+	height = 1
+	for h in range(5):
+		width = 1
+		length = 1
+		height = 1
+		for i in range(10):
+			pyrosim.Send_Cube(name="Box", pos=[z+g,x+h,y+i], 
+size=[length,width,height])
+			width *= .9
+			length *= .9
+			height *= .9
 
 pyrosim.End()
 
