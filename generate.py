d35a2d133c3c6b832934c38f2ec6e2035c96b936
atlibby <atlibby@uvm.edu>
2023-01-30 15:55:20 -0500

    World link in background

--- generate.py
+++ generate.py
@@ -1,21 +1,36 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
+def Create_World():
+	pyrosim.Start_SDF("world.sdf")
 
-width = 1
+	width = 1
 
-length = 1
+	length = 1
 
-height = 1
+	height = 1
 
-x = 0
+	x = 3
 
-y = 0.5
+	y = 0.5
 
-z = 0
+	z = -1
 
-pyrosim.Send_Cube(name="Box", pos=[z,x,y], size=[length,width,height])
+	pyrosim.Send_Cube(name="Box", pos=[z,x,y], size=[length,width,height])
 
-pyrosim.End()
+	pyrosim.End()
 
+def Create_Robot():
+	pyrosim.Start_URDF("body.urdf")
+	width = 1
+	length = 1
+	height = 1
+	x = 0
+	y = 0.5
+	z = 0
+	pyrosim.Send_Cube(name="Torso", pos=[z,x,y], size=[length,width,height])
+	pyrosim.End()
+
+
+Create_World()
+Create_Robot()
 
