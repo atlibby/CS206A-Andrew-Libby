4508d0ee2ea8352f6dc5044114282a7e533fe9e4
atlibby <atlibby@uvm.edu>
2023-02-06 16:02:27 -0500

    Robot joints finally work

--- generate.py
+++ generate.py
@@ -53,16 +53,14 @@ def Create_Bot():
 	width = 1
 	length = 1
 	height = 1
-	x = 0
-	y = 0.5
-	z = 0
-	pyrosim.Send_Cube(name="Torso", pos=[z,x,1.5], size=[length,width,height])
+	pyrosim.Send_Cube(name="Torso", pos=[1.0,1.0,1.5], size=[length,width,height])
 	pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", 
-type="revolute", position=[1,0,1])
-	pyrosim.Send_Cube(name="BackLeg", pos=[0,0,-0.5], size=[length,width,height])
+type="revolute", position=[1.5,0.5,1])
+	pyrosim.Send_Cube(name="BackLeg", pos=[0.5,0.5,-0.5], size=[length,width,height])
 	pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", 
-type="revolute", position=[-1,0,1])
-	pyrosim.Send_Cube(name="FrontLeg", pos=[0,0,-0.5], size=[length,width,height])
+type="revolute", position=[0.5,0.5,1])
+	pyrosim.Send_Cube(name="FrontLeg", pos=[-0.5,0.5,-0.5], 
+size=[length,width,height])
 	pyrosim.End()
 	
 Create_World()
