3f810a3e3f73b27116d96a8ae62aeee86048a0d0
atlibby <atlibby@uvm.edu>
2023-02-14 00:07:11 -0500

    This don't work yet lmao

--- analyze.py
+++ analyze.py
@@ -1,14 +1,19 @@
 import numpy
 import matplotlib.pyplot
+import matplotlib.pylab as plt
 
 backLegSensorValues  = numpy.load('data/backLegSensorValues.npy')
 
 frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
 
-matplotlib.pyplot.plot(frontLegSensorValues, label='Front Leg')
+targetAngleMotorValues = numpy.load('data/targetAngleData.npy')
 
-matplotlib.pyplot.plot(backLegSensorValues, label='Back Leg', linewidth=2.0)
+#matplotlib.pyplot.plot(frontLegSensorValues, label='Front Leg')
 
-matplotlib.pyplot.legend()
+#matplotlib.pyplot.plot(backLegSensorValues, label='Back Leg', linewidth=2.0)
 
-matplotlib.pyplot.show()
+plt.plot(targetAngleMotorValues,numpy.sin(targetAngleMotorValues),label='Angle Data')
+
+plt.legend()
+
+plt.show()
